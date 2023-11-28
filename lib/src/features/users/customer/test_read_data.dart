90a565df11c19e1162c327b25b474dbdc6bcb98e
gan0425 <ganyou@graduate.utm.my>
2023-11-28 20:45:49 +0800

    update test read data

--- lib/src/features/users/customer/test_read_data.dart
+++ lib/src/features/users/customer/test_read_data.dart
@@ -1,56 +1,66 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/services/auth/auth_service.dart';
-import 'package:path/path.dart';
 
-class testRead extends StatefulWidget {
-  const testRead({super.key});
+class TestRead extends StatefulWidget {
+  const TestRead({super.key});
 
   @override
-  State<testRead> createState() => _testReadState();
+  State<TestRead> createState() => _TestReadState();
 }
 
-class _testReadState extends State<testRead> {
-
-  
-
+class _TestReadState extends State<TestRead> {
   @override
   Widget build(BuildContext context) {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
     final userId = AuthService.firebase().currentUser?.id;
     final Stream<DocumentSnapshot> _users = FirebaseFirestore
-    .instance
-    .collection('users').
-    doc(userId).
-    snapshots();
+        .instance
+        .collection('users')
+        .doc(userId)
+        .snapshots();
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('read data'),
+        title: const Text('Read Data'),
+        backgroundColor: Colors.blue, // Set the app bar color
       ),
-      body:
-        StreamBuilder<DocumentSnapshot>(
-          stream: _users, 
-          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot){
-            if(!snapshot.hasData){
-              return Text('Loading');
-            }else if(snapshot.hasError){
-              return Text('Error');
-            }else if(snapshot.connectionState == ConnectionState.waiting){
-              return CircularProgressIndicator();
-            }
-            dynamic data = snapshot.data;
-            return Column(
-              children: [
-                Text(data['fullName']),
-                Text(data['phone']),
-                Text(data['email']),
-              ],
-            );
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: _users,
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: Text('Loading'));
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error'));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
           }
-        )
+          dynamic data = snapshot.data;
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Full Name: ${data['fullName']}',
+                style: TextStyle(color: Colors.green, fontSize: 20),
+              ),
+              Text(
+                'Phone: ${data['phone']}',
+                style: TextStyle(color: Colors.orange, fontSize: 20),
+              ),
+              Text(
+                'Email: ${data['email']}',
+                style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+        
       // FutureBuilder<DocumentSnapshot>(
       //   future: users.doc(userId).get(), 
       //   builder: 
@@ -71,8 +81,6 @@ class _testReadState extends State<testRead> {
 
       //   }
       // ),
-    );
-    
-  }
-}
+ 
+ 
 
