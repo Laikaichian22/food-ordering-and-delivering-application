c4e1ef74cfd5369d9dc8830da2c8d0c8b6891831
gan0425 <ganyou@graduate.utm.my>
2023-12-02 23:42:04 +0800

    Update delivery_homepage.dart

--- lib/src/features/users/deliveryman/delivery_homepage.dart
+++ lib/src/features/users/deliveryman/delivery_homepage.dart
@@ -6,63 +6,66 @@ import 'package:flutter_application_1/src/features/users/business_owner/owner_ho
 import 'package:flutter_application_1/src/routing/routes_const.dart';
 import 'package:flutter_application_1/services/auth/auth_service.dart';
 
-class DeliveryHomePage extends StatefulWidget {
-  const DeliveryHomePage({super.key});
 
-  @override
-  State<DeliveryHomePage> createState() => _DeliveryHomePageState();
-}
+class DeliveryManHomePage extends StatelessWidget {
+  const DeliveryManHomePage({Key? key}) : super(key: key);
 
-class _DeliveryHomePageState extends State<DeliveryHomePage> {
   @override
   Widget build(BuildContext context) {
-    final currentUser = AuthService.firebase().currentUser!;
-    final userID = currentUser.id;
-
-    return SafeArea(
-      child: Scaffold(
-        drawer: DrawerFunction(userId: userID),
-        appBar: AppBar(
-          backgroundColor: deliveryColor, // Assuming you have a color for delivery
-          elevation: 0.0,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              CardWidget(
-                title: 'Delivery Requests',
-                iconBtn: Icons.delivery_dining_outlined,
-                subTitle: 'View and manage delivery requests',
-                cardColor: Colors.green, // Customize the color
-                onTap: () {
-                  // Add functionality for delivery requests
-                },
-              ),
-              CardWidget(
-                title: 'Delivery History',
-                iconBtn: Icons.history,
-                subTitle: 'View completed deliveries',
-                cardColor: Colors.blue, // Customize the color
-                onTap: () {
-                  // Add functionality for delivery history
-                },
-              ),
-              CardWidget(
-                title: 'Delivery Status',
-                iconBtn: Icons.directions_car,
-                subTitle: 'Track the status of ongoing deliveries',
-                cardColor: Colors.orange, // Customize the color
-                onTap: () {
-                  // Add functionality for delivery status
-                },
-              ),
-              // Add more cards for additional features related to delivery
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UmaiFood'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              // TODO: Implement the menu functionality
+            },
           ),
-        ),
+        ],
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Center(
+                child: TextButton(
+                  child: Text(
+                    'Start Delivery',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: () {
+                    // TODO: Implement the start delivery functionality
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.yellow,
+              child: Center(
+                child: TextButton(
+                  child: Text(
+                    'Delivery List',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: () {
+                    // TODO: Implement the delivery list functionality
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-// The CardWidget class remains the same as in the original code
